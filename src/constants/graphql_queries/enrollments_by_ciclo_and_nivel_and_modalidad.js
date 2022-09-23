8d4942c3643fe68a9cfb9fe5160550777ccf194d
Alvaro Rene <alvaro.kom@gmail.com>
2022-09-23 15:09:10 -0500

    fixed graphql query

--- src/constants/graphql_queries/enrollments_by_ciclo_and_nivel_and_modalidad.js
+++ src/constants/graphql_queries/enrollments_by_ciclo_and_nivel_and_modalidad.js
@@ -1,12 +1,20 @@
-import { gql } from 'apollo-boost';
+import { gql } from "apollo-boost";
 
 export const ENROLLMENTS_BY_CICLO_AND_NIVEL_AND_MODALIDAD = gql`
-	query GrupoEnrollmentByCicloAndNivelAndModalidad($idCiclo: String!,$idNivel: String!,$idModalidad: String!) {
-		  grupoEnrollmentByCicloAndNivelAndModalidad(idCiclo: $idCiclo,idNivel:$idNivel, idModalidad: $idModalidad, idGrado: $idGrado) {
-			id
-			idPersona
-			nombre
-			tipo
-		}
-	}
+  query GrupoEnrollmentByCicloAndNivelAndModalidad(
+    $idCiclo: String!
+    $idNivel: String!
+    $idModalidad: String!
+  ) {
+    grupoEnrollmentByCicloAndNivelAndModalidad(
+      idCiclo: $idCiclo
+      idNivel: $idNivel
+      idModalidad: $idModalidad
+    ) {
+      id
+      idPersona
+      nombre
+      tipo
+    }
+  }
 `;
